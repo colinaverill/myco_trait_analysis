54b2e1a6c02bc48726b31bd18d0a7de8577eb412
colin.averill <colin.averill@gmail.com>
2018-12-26 15:12:54 -0500

    only using forest - non.forest and latitudinal zone as biomes, running aicc.

--- data_analysis/aicc_comparison/3._forest.lat_pgls_aicc.r
+++ data_analysis/aicc_comparison/3._forest.lat_pgls_aicc.r
@@ -9,7 +9,7 @@ source('functions/tic_toc.r')
 source('functions/pgls_step_aicc.r')
 
 #set output path.----
-output.path <- aicc_lat_interactions_biome_pgls.path
+output.path <- aicc_lat.forest_interactions_biome_pgls.path
 
 #load data.----
 d <- readRDS(inter_specific_analysis_data.path)
@@ -23,27 +23,37 @@ rownames(d) <- d$Species
 d <- d[d$Species %in% phy$tip.label,] #drops 21 species.
 setnames(d,'root_lifespan','root.L')
 
+#Divide MAP by 100 (should improve algo convergence.)
+d$map.c <- d$map.c / 100
+
 
 #get factors as 0-1 variables for AIC selection.----
 #set temperate latitudes and temperate forests as base groups.
-d.fac <- d[,c('biome2','biome3')]
-d.fac$biome2 <- ifelse(d$biome2 == 'Temperate_forest','a_Temperate_forest',d.fac$biome2)
-#biome 3 already preceded by a_.
+d.fac <- data.frame(d[,c('biome3')])
+colnames(d.fac) <- c('biome3')
 #make sure they actually factors.
 d.fac[] <- lapply(d.fac, as.factor)
 #covert to 0-1 matrix by factor level (save for base group).
 options(na.action='na.pass')
-fac.num <- model.matrix( ~ biome2 + biome3, data=d.fac)
+fac.num <- model.matrix( ~  biome3, data=d.fac)
 fac.num <- fac.num[,-1] #drop intercept column.
+
+#flip forest predictor so 0 (reference) is forested.
+d$forest <- abs(d$forest - 1)
 
 #make dataset for analysis.----
 d <- data.table::data.table(d)
-d <- d[,.(Species,MYCO_ASSO,pgf,nfix,mat.c,map.c,Ngreen,Pgreen,Nsenes,Psenes,Nroots,Proots,log.LL,root.L)]
+d <- d[,.(Species,MYCO_ASSO,pgf,nfix,forest,mat.c,map.c,Ngreen,Pgreen,Nsenes,Psenes,Nroots,Proots,log.LL,root.L)]
 d$MYCO_ASSO <- ifelse(d$MYCO_ASSO == 'ECM',1,0)
 d$pgf <- ifelse(d$pgf == 'gymno',1,0)
 d <- cbind(d,fac.num)
+d$non.forest_myco <- d$MYCO_ASSO * d$forest
 d$myco_tropical <- d$MYCO_ASSO * d$biome3b_tropical
 d$myco_boreal   <- d$MYCO_ASSO * d$biome3c_boreal
+d$non.forest_tropical <- d$forest * d$biome3b_tropical
+d$non.forest_boreal <- d$forest * d$biome3c_boreal
+d$non.forest_tropical_ECM <- d$forest * d$biome3b_tropical * d$MYCO_ASSO
+d$non.forest_boreal_ECM <- d$forest * d$biome3c_boreal * d$MYCO_ASSO
 
 #get predictor names.----
 not_preds <- c('Species','Ngreen','Pgreen','Nsenes','Psenes','Nroots','Proots','log.LL','root.L')
